90fb67aea8ad23ea648e4144f77ef3d703274b3c
Md Naimul Hasan Shadesh <96987942+mdshadesh@users.noreply.github.com>
2022-05-25 22:47:44 +0600

    Add files via upload

---
+++ submission-842898-source.cpp
@@ -0,0 +1,31 @@
+// Naimul Hasan SHadesh
+// Sonargaon University
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	string s;
+	cin >> s;
+
+	int con = 0;
+	for(int i = 0; i < s.length(); i++)
+	{
+		switch(tolower(s[i]))
+		{
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				con++;
+				break;
+		}
+	}
+
+	cout << con << endl;
+
+	return 0;
+}
---
+++ submission-842913-source.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc,size,arr[1001];
+    cin>>tc;
+
+    for(int i=0;i<tc;i++)
+    {
+        cin>>size;
+        for(int j=0;j<size;j++)
+        {
+            cin>>arr[j];
+        }
+        for(int j=0;j<size;j++)
+        {
+
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+	return 0;
+}
+
+
